f6c826d90512f1b5533b159d42ebe57e5cd813b0
nyccollas <115283543+nyccollas@users.noreply.github.com>
2025-07-06 18:58:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let button = document.querySelector("#button");
+let text = document.querySelector("#mensagem");
+
+button.addEventListener("click", () => {
+    text.innerHTML = '- Seja Você.';
+})
